27d96d1bf7a261c742393131845f502b0b4067d7
Jason Moix <jason.moix@gmail.com>
2015-12-01 10:40:07 -0600

    add soccer ball default team image

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utilies.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utilies.java
@@ -49,7 +49,7 @@ public class Utilies
     }
 
     public static int getTeamCrestByTeamName (String teamname) {
-        if (teamname==null){return R.drawable.no_icon;}
+        if (teamname==null){return R.drawable.soccerball;}
 
         switch (teamname) { //This is the set of icons that are currently in the app. Feel free to find and add more
             //as you go.
@@ -63,7 +63,7 @@ public class Utilies
             case "West Bromwich Albion" : return R.drawable.west_bromwich_albion_hd_logo;
             case "Sunderland AFC" : return R.drawable.sunderland;
             case "Stoke City FC" : return R.drawable.stoke_city;
-            default: return R.drawable.no_icon;
+            default: return R.drawable.soccerball;
         }
     }
 }
